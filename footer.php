8ca43e60dce687d02199e80994b8f89cb4254092
N-5 <yfe63590@gmail.com>
2018-09-05 11:06:51 +0900

    footer direct link

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 </div><!-- end barba-container-->
 </div><!-- end barba-wrapper-->
 <div class="c-fixlist">
-  <div class="fixlist_item"><a href="<?php echo home_url(); ?>/entry"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/fix_menu_01.jpg" alt=""></a></div>
+  <div class="fixlist_item"><a href="<?php echo home_url(); ?>/entry/?post_id=<?php echo $post->ID; ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/fix_menu_01.jpg" alt=""></a></div>
   <div class="fixlist_item"><a href="https://line.me/R/ti/p/c7-u3URgu1" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/fix_menu_02.jpg" alt=""></a></div>
 </div>
 <footer class="footer">
